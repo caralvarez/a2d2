d6ef72b678814baf39bc1f7800083ec74c3d5ef8
Mariano De Maio <marianbuenosayres@gmail.com>
2023-08-10 10:58:00 -0300

    Paging placeholder branch

--- fhir-query-helper-base/src/main/java/io/elimu/a2d2/cds/fhir/helper/PagingHelper.java
+++ fhir-query-helper-base/src/main/java/io/elimu/a2d2/cds/fhir/helper/PagingHelper.java
@@ -12,30 +12,70 @@ public class PagingHelper {
 
 	private static final Logger LOG = LoggerFactory.getLogger(PagingHelper.class);
 	
-	public static final String PAGE_ID_PARAM = "_getpages";
-	public static final String PAGE_OFFSET_PARAM = "_getpagesoffset";
-	public static final String PAGE_SIZE_PARAM = "_count";
+	public static final String PAGE_ID_PARAM_DEFAULT = "_getpages";
+	public static final String PAGE_OFFSET_PARAM_DEFAULT = "_getpagesoffset";
+	public static final String PAGE_SIZE_PARAM_DEFAULT = "_count";
+
+	public static class Builder {
+		private String pageIdParam = PAGE_ID_PARAM_DEFAULT;
+		private String pageOffsetParam = PAGE_OFFSET_PARAM_DEFAULT;
+		private String pageSizeParam = PAGE_SIZE_PARAM_DEFAULT;
+		
+		public Builder withPageIdParam(String name) {
+			pageIdParam = name;
+			return this;
+		}
+		
+		public Builder withPageOffsetParam(String name) {
+			pageOffsetParam = name;
+			return this;
+		}
+		
+		public Builder withPageSizeParam(String name) {
+			pageSizeParam = name;
+			return this;
+		}
+		
+		public PagingHelper build() {
+			return new PagingHelper(pageIdParam, pageOffsetParam, pageSizeParam);
+		}
+	}
 	
-	public static QueryBuilder pageFromUrl(String url) {
+	private final String pageIdParam;
+	private final String pageOffsetParam;
+	private final String pageSizeParam;
+	
+	public PagingHelper() {
+		this(PAGE_ID_PARAM_DEFAULT, PAGE_OFFSET_PARAM_DEFAULT, PAGE_SIZE_PARAM_DEFAULT);
+	}
+	
+	public PagingHelper(String pageIdParam, String pageOffsetParam, String pageSizeParam) {
+		super();
+		this.pageIdParam = pageIdParam;
+		this.pageOffsetParam = pageOffsetParam;
+		this.pageSizeParam = pageSizeParam;
+	}
+
+	public QueryBuilder pageFromUrl(String url) {
 		QueryBuilder retval = new QueryBuilder();
 		populatePageFromUrl(retval, url);
 		return retval;
 	}
 	
-	public static QueryBuilder nextPageFromUrl(String url) {
+	public QueryBuilder nextPageFromUrl(String url) {
 		QueryBuilder retval = new QueryBuilder();
 		populateNextPageFromUrl(retval, url);
 		return retval;
 	}
 	
-	public static void populatePageFromUrl(QueryBuilder qb, String url) {
-		String ssize = extractParam(url, PAGE_SIZE_PARAM);
-		String soffset = extractParam(url, PAGE_OFFSET_PARAM);
-		String pageId = extractParam(url, PAGE_ID_PARAM);
+	public void populatePageFromUrl(QueryBuilder qb, String url) {
+		String ssize = extractParam(url, pageSizeParam);
+		String soffset = extractParam(url, pageOffsetParam);
+		String pageId = extractParam(url, pageIdParam);
 		Map<String, List<String>> params = new HashMap<>();
-		params.put(PAGE_ID_PARAM, Arrays.asList(pageId));
-		params.put(PAGE_OFFSET_PARAM, Arrays.asList(soffset));
-		params.put(PAGE_SIZE_PARAM, Arrays.asList(ssize));
+		params.put(pageIdParam, Arrays.asList(pageId));
+		params.put(pageOffsetParam, Arrays.asList(soffset));
+		params.put(pageSizeParam, Arrays.asList(ssize));
 		populatePageFromUrl(qb, params);
 	}
 
@@ -53,29 +93,29 @@ public class PagingHelper {
 		return null;
 	}
 	
-	public static void populatePageFromUrl(QueryBuilder qb, Map<String, List<String>> parameters) {
-		String ssize = parameters.getOrDefault(PAGE_SIZE_PARAM, Arrays.asList((String) null)).get(0);
-		String soffset = parameters.getOrDefault(PAGE_OFFSET_PARAM, Arrays.asList((String) null)).get(0);
-		String pageId = parameters.getOrDefault(PAGE_ID_PARAM, Arrays.asList((String) null)).get(0);
+	public void populatePageFromUrl(QueryBuilder qb, Map<String, List<String>> parameters) {
+		String ssize = parameters.getOrDefault(pageSizeParam, Arrays.asList((String) null)).get(0);
+		String soffset = parameters.getOrDefault(pageOffsetParam, Arrays.asList((String) null)).get(0);
+		String pageId = parameters.getOrDefault(pageIdParam, Arrays.asList((String) null)).get(0);
 		qb.pageId(pageId);
 		if (ssize != null) {
 			try {
 				qb.count(Integer.valueOf(ssize));
 			} catch (Exception e) { 
-				LOG.warn("Problem parsing " + PAGE_SIZE_PARAM + " from value " + ssize + ": " + e.getMessage());
+				LOG.warn("Problem parsing " + pageSizeParam + " from value " + ssize + ": " + e.getMessage());
 			}
 		}
 		if (ssize != null) {
 			try {
 				qb.offset(Integer.valueOf(soffset));
 			} catch (Exception e) { 
-				LOG.warn("Problem parsing " + PAGE_OFFSET_PARAM + " from value " + soffset + ": " + e.getMessage());
+				LOG.warn("Problem parsing " + pageOffsetParam + " from value " + soffset + ": " + e.getMessage());
 			}
 		}
 
 	}
 
-	public static void populateNextPageFromUrl(QueryBuilder qb, String url) {
+	public void populateNextPageFromUrl(QueryBuilder qb, String url) {
 		populatePageFromUrl(qb, url);
 		int size = qb.getPageSize();
 		int newOffset = qb.getOffset() + size;
